acba8a9a6b5cd13cf16967e81442ac3062ae8ea5
Vedant Darokar <vedantdarokar7@gmail.com>
2026-01-19 22:07:31 +0530

    changes done

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Singleton socket connection
-const socket = io("http://localhost:5000");
+const socket = io("https://hotel-qr-system.onrender.com");
 
 export default socket;
